c65f65b49a27304b3b320350eee4d690ec0ee94c
diyaliza <dvarghes@asu.edu>
2024-01-06 10:57:42 -0700

    files

---
+++ local_to_s3.py
@@ -0,0 +1,22 @@
+import boto3
+s3_resource = boto3.resource('s3','us-west-2')
+
+def s3_upload(file_name, fold, bkt):
+    s3_bucket = s3_resource.Bucket(name=bkt)
+    if True:
+        s3_bucket.upload_file(
+            Filename = file_name,
+            Key = fold + '/' + file_name
+        )
+    return True
+
+if __name__=='__main__':
+    file_name = 'customer_data.csv'
+    s3_folder = 'raw_data'
+    bucket = 'snowflake-project-etl'
+
+    status = s3_upload(file_name, s3_folder, bucket)
+    if(status):
+        print('Data saved')
+    else:
+        print('Error')
